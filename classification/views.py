79b49f0c3951438f85056d8eb3e7923809aa41b9
Jeff Wang <jeff.wang@ribitwo.com>
2018-11-01 15:33:52 +0800

    classification image result support

--- classification/views.py
+++ classification/views.py
@@ -6,8 +6,7 @@ from django.views.decorators.csrf import csrf_exempt,csrf_protect
 
 import urllib
 # If you are using Python 3+, import urllib instead of urllib2
-
-import json 
+import json
 
 from pyecharts import Bar
 
@@ -20,8 +19,11 @@ def index(request):
     data = np.zeros((1, 4))
     res_class = ''
     echart = ''
-    #result = ''
+    # result = ''
     class_dict = {0:'Setosa', 1:'Versicolor', 2:'Virginica'}
+    res_img_dict = {0:'img/setosa.jpg',
+                    1:'img/versicolor.jpg',
+                    2:'img/virginica.jpg'}
     if request.method == 'POST':
         data[0][0] = float(request.POST['Col1'])
         data[0][1] = float(request.POST['Col2'])
@@ -29,23 +31,30 @@ def index(request):
         data[0][3] = float(request.POST['Col4'])
         result = classify(data)
        
-        return render(request, 'index.html', {'res_class': class_dict[result[0]]})
+        return render(request, 'index.html', {'res_class': class_dict[result[0]], 'res_img': res_img_dict[result[0]],
+                                              'feat1': data[0][0],
+                                              'feat2': data[0][1],
+                                              'feat3': data[0][2],
+                                              'feat4': data[0][3]})
     else:
-    	return render(request, 'index.html', {'res_class': ''})
+        return render(request, 'index.html', {'res_class': '', 'res_img': 'img/iris.jpg',
+                                              'feat1': 5.1,
+                                              'feat2': 3.5,
+                                              'feat3': 5.4,
+                                              'feat4': 0.2
+                                              })
 
 
 def classify(data):
-	       
-	
-	result = svc.predict(data)    
-	
-	print(result)
-	result_proba = svc.predict_proba(data) 
-	bar =Bar("每种分类结果的概率")
-	bar.add("分类结果", ["Iris-setosa", "Iris-versicolor", "Iris-virginica"], 
-	        [result_proba[0][0], result_proba[0][1], 
-	         result_proba[0][2]]
-	       )
-	bar.show_config()
-	bar.render("./classification/templates/echarts.html")
-	return result
+    result = svc.predict(data)
+
+    print(result)
+    result_proba = svc.predict_proba(data)
+    bar =Bar("每种分类结果的概率")
+    bar.add("分类结果", ["Iris-setosa", "Iris-versicolor", "Iris-virginica"],
+           [round(result_proba[0][0], 2), round(result_proba[0][1], 2),
+            round(result_proba[0][2], 2)]
+            )
+    # bar.show_config()
+    bar.render("./classification/templates/echarts.html")
+    return result
